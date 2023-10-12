c7835889bf039784b17ee3f6930c9f90ad8f53cb
pieterjm <pieter@meulenhoff.org>
2023-10-12 17:25:40 +0200

    Fixed bug due to Lnbits update

--- lnbits/bliksembier/crud.py
+++ lnbits/bliksembier/crud.py
@@ -53,7 +53,7 @@ async def update_device(
             if _switch.id is None:
                 _switch.id = shortuuid.uuid()[:8]            
                 _switch.lnurl = lnurl_encode(
-                    url
+                    str(url)
                     + "?switch_id="
                     + str(_switch.id)
                 )
